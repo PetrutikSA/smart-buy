96fdc5943e0e7b99ba6f112f82f787473de14e1d
Stanislav Petrutik <petrutiksa@yandex.ru>
2025-01-15 12:38:22 +0300

    feat: initial commit

---
+++ src/test/java/ru/petrutik/smart_buy/SmartBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petrutik.smart_buy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
