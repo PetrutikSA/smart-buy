ede857998168e4e7015736813ec86fd5b9d47f0b
Stanislav Petrutik <petrutiksa@yandex.ru>
2025-01-17 14:53:54 +0300

    feat: add request service spring boot module

---
+++ smart-buy-request-service/src/main/java/ru/petrutik/smartbuy/requestservice/SmartBuyRequestServiceApp.java
@@ -0,0 +1,11 @@
+package ru.petrutik.smartbuy.requestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBuyRequestServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBuyRequestServiceApp.class, args);
+    }
+}
