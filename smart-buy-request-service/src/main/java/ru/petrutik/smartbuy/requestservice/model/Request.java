dd9379b35cda112cdde5015fadbbc3373d67b6ba
Stanislav Petrutik <petrutiksa@yandex.ru>
2025-01-20 16:41:36 +0300

    feat: add schema.sql

--- smart-buy-request-service/src/main/java/ru/petrutik/smartbuy/requestservice/model/Request.java
+++ smart-buy-request-service/src/main/java/ru/petrutik/smartbuy/requestservice/model/Request.java
@@ -1,12 +1,13 @@
 package ru.petrutik.smartbuy.requestservice.model;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class Request {
     private Long id;
     private String name;
     private String searchQuery;
-    private String maxPrice;
+    private BigDecimal maxPrice;
     private boolean isUpdated;
     private List<Product> result;
     private List<Product> banned;
