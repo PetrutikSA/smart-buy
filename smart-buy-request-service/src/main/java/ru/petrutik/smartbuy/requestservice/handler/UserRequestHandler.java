291c2f7a068991275a9dd677943a9a11e7f7deb6
Stanislav Petrutik <petrutiksa@yandex.ru>
2025-01-21 16:12:49 +0300

    fix: kafka connect from docker compose

--- smart-buy-request-service/src/main/java/ru/petrutik/smartbuy/requestservice/handler/UserRequestHandler.java
+++ smart-buy-request-service/src/main/java/ru/petrutik/smartbuy/requestservice/handler/UserRequestHandler.java
@@ -1,7 +1,10 @@
 package ru.petrutik.smartbuy.requestservice.handler;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
+@Component
 @KafkaListener(topics = "#{@kafkaConfig.getRequestTopicName()}")
 public class UserRequestHandler {
+
 }
