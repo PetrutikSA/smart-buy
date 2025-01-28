d80d138d0880cb78998707782abcc6e653a91aec
Stanislav Petrutik <petrutiksa@yandex.ru>
2025-01-28 13:20:59 +0300

    fix: parse service add spring boot module

---
+++ smart-buy-parse-service/src/main/java/ru/petrutik/samrtbuy/parseservice/SmartBuyParseService.java
@@ -0,0 +1,11 @@
+package ru.petrutik.samrtbuy.parseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBuyParseService {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBuyParseService.class, args);
+    }
+}
