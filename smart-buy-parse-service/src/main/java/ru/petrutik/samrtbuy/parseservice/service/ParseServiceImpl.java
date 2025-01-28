5b08c1a375eaac23b9918469952cb69ab0072874
Stanislav Petrutik <petrutiksa@yandex.ru>
2025-01-28 15:50:26 +0300

    feat: parse service send add response to kafka topic

--- smart-buy-parse-service/src/main/java/ru/petrutik/samrtbuy/parseservice/service/ParseServiceImpl.java
+++ smart-buy-parse-service/src/main/java/ru/petrutik/samrtbuy/parseservice/service/ParseServiceImpl.java
@@ -1,24 +1,41 @@
 package ru.petrutik.samrtbuy.parseservice.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 import ru.petrutik.smartbuy.event.dto.ProductDto;
+import ru.petrutik.smartbuy.parse.response.AddResponseParseEvent;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ParseServiceImpl implements ParseService {
     private final List<ParseMarketService> parseMarketServices;
+    private final KafkaTemplate<Long, Object> kafkaTemplate;
+    private final String responseTopicName;
+    private final Logger logger;
 
-    public ParseServiceImpl(List<ParseMarketService> parseMarketServices) {
+    public ParseServiceImpl(List<ParseMarketService> parseMarketServices,
+                            KafkaTemplate<Long, Object> kafkaTemplate,
+                            @Value("#{@kafkaConfig.getResponseTopicName()}") String responseTopicName) {
         this.parseMarketServices = parseMarketServices;
+        this.kafkaTemplate = kafkaTemplate;
+        this.responseTopicName = responseTopicName;
+        this.logger = LoggerFactory.getLogger(ParseServiceImpl.class);
     }
 
     @Override
     public void addRequestParsing(Long requestId, String searchQuery, BigDecimal maxPrice) {
         List<ProductDto> products = parseQuery(searchQuery, maxPrice);
+        AddResponseParseEvent addResponseParseEvent = new AddResponseParseEvent(requestId, products);
+        sendToKafkaTopic(requestId, addResponseParseEvent);
     }
 
     private List<ProductDto> parseQuery(String searchQuery, BigDecimal maxPrice) {
@@ -31,5 +48,17 @@ public class ParseServiceImpl implements ParseService {
                 .sorted(Comparator.comparing(ProductDto::getPrice))
                 .limit(5)
                 .toList();
+    }
+
+    private void sendToKafkaTopic(Long key, Object value) {
+        CompletableFuture<SendResult<Long, Object>> future =
+                kafkaTemplate.send(responseTopicName, key, value);
+        future.whenComplete(((stringSendResult, throwable) -> {
+            if (throwable != null) {
+                logger.error("Failed to send message: {}", throwable.getLocalizedMessage(), throwable);
+            } else {
+                logger.info("Message sent successfully {}", stringSendResult);
+            }
+        }));
     }
 }
---
+++ user-request-core/src/main/java/ru/petrutik/smartbuy/parse/response/AddResponseParseEvent.java
@@ -0,0 +1,26 @@
+package ru.petrutik.smartbuy.parse.response;
+
+import ru.petrutik.smartbuy.event.dto.ProductDto;
+
+import java.util.List;
+
+public class AddResponseParseEvent {
+    private Long requestId;
+    private List<ProductDto> products;
+
+    public AddResponseParseEvent() {
+    }
+
+    public AddResponseParseEvent(Long requestId, List<ProductDto> products) {
+        this.requestId = requestId;
+        this.products = products;
+    }
+
+    public Long getRequestId() {
+        return requestId;
+    }
+
+    public List<ProductDto> getProducts() {
+        return products;
+    }
+}
